12900a5097d9c37277300b5b51a92427d760846e
Mosan <7314041+gctmt@users.noreply.github.com>
2021-02-03 15:13:37 +0000

    Create python.rb

---
+++ Casks/python.rb
@@ -0,0 +1,21 @@
+cask "python" do
+  version "3.9.1"
+  sha256 "8f8782d278e3926566cba11182453379568dc98d82adde9ef38e28439d46f736"
+  
+  shasum256url "https://nodejs.org/dist/v#{version}/SHASUMS256.txt"
+  url "https://www.python.org/ftp/python/#{version}/python-#{version}-macos11.0.pkg"
+  
+  appcast "https://www.python.org/downloads/mac-osx/"
+  name "Python"
+  homepage "https://www.python.org/"
+
+  # conflicts_with cask: ["python2"]
+  #               formula:["minimal-racket"]}
+  
+  pkg "python-#{version}-macos11.0.pkg"
+
+  uninstall pkgutil: ["org.python.Python.PythonUnixTools-3.9"
+                      ,"org.python.Python.PythonApplications-3.9"
+                      ,"org.python.Python.PythonDocumentation-3.9"
+                      ,"org.python.Python.PythonFramework-3.9"]
+end
